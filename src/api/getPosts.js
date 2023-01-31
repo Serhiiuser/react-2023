7633936dac2ccf21c8805ab1deb6d362771dd982
Serhii Kobryn <serhiik91@gmail.com>
2023-01-31 15:46:48 +0200

    main fix

--- src/api/getPosts.js
+++
@@ -1,8 +0,0 @@
-export const getPosts = ()=> {
-    return fetch('https://jsonplaceholder.typicode.com/posts').then(resp => resp.json());
-
-}
-export const getPost = (id,title)=> {
-    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(resp => resp.json());
-
-}
