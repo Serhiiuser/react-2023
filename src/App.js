80488f51c40abff55c4a8c0caf87fef49fc41003
Serhii Kobryn <serhiik91@gmail.com>
2023-01-31 16:30:12 +0200

    part2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
+    const [launches, setLaunches] = useState([]);
+    useEffect(()=>{
+        fetch('https://api.spacexdata.com/v3/launches/').then(resp => resp.json()).then(resp => setLaunches(resp))
+    },[]);
+
   return (
-      <div>
+      <div className={'App'}>
+          {launches.filter(item => item.launch_year !=='2020').map(item => {
+              return(
+                  <div key={item.flight_number}>
+
+                      {item.flight_number} --- {item.mission_name} --- {item.launch_year}
+                  </div>
+              )
+          })}
         
       </div>
   );
